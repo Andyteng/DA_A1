17d45f670d310c294b4693e3926f032c8520ee73
lmf5103510 <lmf5103510@126.com>
2016-12-03 13:37:39 +0100

    Lab 1 frozen

--- Ass1/Cmd/Process2.java
+++ Ass1/Cmd/Process2.java
@@ -1,30 +1,36 @@
 import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
 public class Process2 {
 
 	static int i =2;
 	public static void main(String[] args) throws Exception{
+		DA_BSS daimp;
+		System.setSecurityManager(new RMISecurityManager());
+		try{
+			LocateRegistry.createRegistry(1097);   //Do the registry
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
+		daimp = new DA_BSS(i);
+		Naming.rebind("rmi://localhost:1097/DA"+i, daimp);
 		
 		DA_BSS_RMI proc = (DA_BSS_RMI) Naming.lookup("rmi://localhost:1097/DA"+i);        //Now we have Total_Process_Num of processes
-		proc.setProcessesNetwork();
 		
-		Thread tr = new Thread("Main_"+i){
-			public void run(){
-				try{
-					Thread.sleep(1200);
-					proc.setLocalVector(i);
-					Messages testmsg = new Messages();                //Define the message to be sent
-					testmsg.msg = "the Message P"+i;
-					testmsg.idSender = i;
-					testmsg.vectorClock = proc.getLocalVector();
-					proc.broadcastMessage(testmsg);
-				}
-				catch (Exception e) {
-						// TODO: handle exception
-					e.printStackTrace();
-				}
-			}
-		};
-		tr.start();
+		try{
+			Thread.sleep(2200);	
+			proc.setProcessesNetwork();
+			proc.setLocalVector(i);
+			Messages testmsg = new Messages();                //Define the message to be sent
+			testmsg.msg = "the Message P"+i;
+			testmsg.idSender = i;
+			testmsg.vectorClock = proc.getLocalVector();
+			proc.broadcastMessage(testmsg);
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 }
--- Ass1/Cmd/Start.bat
+++ Ass1/Cmd/Start.bat
@@ -1,3 +1,3 @@
-start D:\Users\lmf\Desktop\DA\Process0.bat
 start D:\Users\lmf\Desktop\DA\Process1.bat
+start D:\Users\lmf\Desktop\DA\Process0.bat
 start D:\Users\lmf\Desktop\DA\Process2.bat
